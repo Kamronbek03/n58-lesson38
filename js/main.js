3181bf0bbd3d68f03c8f1c8dde9042f756046f14
Kamronbek03 <kamronb3k03@gmail.com>
2024-04-23 00:31:45 +0500

    2-homework

---
+++ js/main.js
@@ -0,0 +1,108 @@
+// Uyga vazifa //
+// N1 //
+// let A;
+// let B;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// let Y = (-B / 2) * (A ** 2 + B ** 2);
+// console.log(Y);
+
+// N2 //
+// let A;
+// let B;
+// let C;
+// let D;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// C = +prompt("C=");
+// D = +prompt("D=");
+// let Y = ((A + B) / (C + D)) ** 2 + B ** 2;
+// console.log(Y);
+
+// N3 //
+// let A;
+// let C;
+// let D;
+// A = +prompt("A=");
+// C = +prompt("C=");
+// D = +prompt("D=");
+// let Y = 2 * (A ** D + (4 * C ** 2) / 3);
+// console.log(Y);
+
+// N4 //
+// let A;
+// let B;
+// let C;
+// let D;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// C = +prompt("C=");
+// D = +prompt("D=");
+// let Y = 1 / A ** 2 + (B / 10) ** 3 + (C + D) ** 2;
+// console.log(Y);
+
+// N5 //
+// let A;
+// let B;
+// let C;
+// let D;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// C = +prompt("C=");
+// D = +prompt("D=");
+// let Y = ((A / B - 1) / (C / (D - 1))) ** 2;
+// console.log(Y);
+
+// N6 //
+// let A;
+// let B;
+// let C;
+// let D;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// C = +prompt("C=");
+// D = +prompt("D=");
+// let Y = (A + B) / (C + D / (A + C));
+// console.log(Y);
+
+// N7 //
+// let A;
+// let B;
+// let C;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// C = +prompt("C=");
+// let Y = Math.sqrt((A + B + C) ** 2 - (A - B - C) ** 2);
+// console.log(Y);
+
+// N8 //
+// let A;
+// let B;
+// let C;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// C = +prompt("C=");
+// let Y = (A ** 2 + B ** 2 + C ** 2) / (A * B * C);
+// console.log(Y);
+
+// N9 //
+// let A;
+// let B;
+// let C;
+// let D;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// C = +prompt("C=");
+// D = +prompt("D=");
+// let Y = A * (D ** 3 / 3) + B * (C ** 2 / 2);
+// console.log(Y);
+
+// N10 //
+// let A;
+// let B;
+// let C;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// C = +prompt("C=");
+// let Y = Math.abs(A / B - B / C) + (A + C) ** (1 / 4); // yoki >>> let Y = Math.abs(A / B - B / C) + Math.sqrt(Math.sqrt(A + C));
+// console.log(Y);
